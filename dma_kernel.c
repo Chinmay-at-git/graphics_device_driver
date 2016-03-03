7a07e773c9e37f31f97c879c118e4153e5f82aec
Shreya Deodhar <sdeodha@g.clemson.edu>
2016-03-03 01:28:49 +0000

    dma_kernel.c cleanup phase 1

--- dma_kernel.c
+++ dma_kernel.c
@@ -19,13 +19,15 @@
 #include <linux/signal.h>
 #include "defs.h"
 
-#define BUFF_SIZE (124*1024)//(19*4)
+#define BUFF_SIZE (124*1024)
+
 #define pci_vendor_ids_CCORSI 0x1234
 
 #define PCI_DEVICE_ID_CCORCI_KYOUKO3 0x1113
 #define Device_RAM 0x0020
 
 #define NO_OF_BUFFS 8
+#define FIFO_ENTRIES 1024
 
 MODULE_LICENSE("Proprietary");
 MODULE_AUTHOR("CS");
@@ -114,7 +116,7 @@ int kyouko3_open(struct inode *inode, struct file *fp)
 	size_ram = size_ram * 1024* 1024;
 	kyouko3.k_card_ram_base = ioremap(kyouko3.p_card_ram,size_ram);
 	kyouko3.user_id = get_current_cred()->uid.val; 
-	printk(KERN_ALERT "User ID is %d",kyouko3.user_id);
+//	printk(KERN_ALERT "User ID is %d",kyouko3.user_id);
 	init_fifo();
 	kyouko3.suspend =0;
 	fifo_flush();
@@ -123,16 +125,6 @@ int kyouko3_open(struct inode *inode, struct file *fp)
 
 int kyouko3_release( struct inode *inode, struct file *fp)
 {
-	/*int i;
-	kyouko3_ioctl(fp,VMODE,GRAPHICS_OFF);
-	fifo_flush();
-	for(i = 0; i < NO_OF_BUFFS; i++)
-     	{	
-		if(kyouko3.buffers_alloted==0)
-				break;
-        	vm_munmap((unsigned long)dma_buffers[i].k_buffer_addr, (size_t) 124*1024);
-        	pci_free_consistent(kyouko3.dev, 124*1024, dma_buffers[i].k_buffer_addr ,*((dma_addr_t*)&dma_buffers[i].handle));
-     	}*/
 	kyouko3.buffers_alloted = 0;
 	kyouko3.suspend = 0;
     pci_free_consistent(kyouko3.dev,8192u,kyouko3.fifo.k_base,*((dma_addr_t*)&kyouko3.fifo.p_base));
@@ -153,31 +145,28 @@ int kyouko3_mmap(struct file *fp,struct vm_area_struct *vma)
 	switch(vma->vm_pgoff << PAGE_SHIFT)
 	{
 		case 0: //control
-			if(kyouko3.user_id!=0)
-			{
-				printk(KERN_ALERT "Only Root is allowed to mmap");
-				return -1;
-			}
-		ret = io_remap_pfn_range(vma, vma->vm_start, kyouko3.p_control_base>>PAGE_SHIFT, vma->vm_end - vma->vm_start, vma->vm_page_prot);
-		break;
+				if(kyouko3.user_id!=0)
+				{
+					printk(KERN_ALERT "Only Root is allowed to mmap");
+					return -1;
+				}
+				ret = io_remap_pfn_range(vma, vma->vm_start, kyouko3.p_control_base>>PAGE_SHIFT, vma->vm_end - vma->vm_start, vma->vm_page_prot);
+				break;
 
 		case 0x80000000: 
-		
-			if(kyouko3.user_id!=0)
-			{	
-		printk(KERN_ALERT "Only Root is allowed to mmap");
-		return -1;
-			}
-		ret = io_remap_pfn_range(vma, vma->vm_start, kyouko3.p_card_ram>>PAGE_SHIFT, vma->vm_end - vma->vm_start, vma->vm_page_prot);
-		
-		break;
+				if(kyouko3.user_id!=0)
+				{	
+					printk(KERN_ALERT "Only Root is allowed to mmap");
+					return -1;
+				}
+				ret = io_remap_pfn_range(vma, vma->vm_start, kyouko3.p_card_ram>>PAGE_SHIFT, vma->vm_end - vma->vm_start, vma->vm_page_prot);
+				break;
 
-		default:
-			ret = remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff, (vma->vm_end)-(vma->vm_start), vma->vm_page_prot);
-			printk(KERN_ALERT "mmap inside %d:",ret);
-		break;
+		default:ret = remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff, (vma->vm_end)-(vma->vm_start), vma->vm_page_prot);
+//				printk(KERN_ALERT "mmap inside %d:",ret);
+				break;
 	}
-		return(ret);
+	return(ret);
 	
 }
 
@@ -186,7 +175,7 @@ void fifo_write(unsigned int reg, unsigned int value)
 	kyouko3.fifo.k_base[kyouko3.fifo.head].command=reg;
 	kyouko3.fifo.k_base[kyouko3.fifo.head].value=value;
 	kyouko3.fifo.head++;
-	if(kyouko3.fifo.head >= 1024)
+	if(kyouko3.fifo.head >= FIFO_ENTRIES)
 	{
 		kyouko3.fifo.head = 0;
 	}
@@ -197,7 +186,7 @@ void fifo_write_F(unsigned int reg, float value)
 	kyouko3.fifo.k_base[kyouko3.fifo.head].command=reg;
 	kyouko3.fifo.k_base[kyouko3.fifo.head].value=*(unsigned int *)&value;
 	kyouko3.fifo.head++;
-	if(kyouko3.fifo.head >= 1024)
+	if(kyouko3.fifo.head >= FIFO_ENTRIES)
 	{
 		kyouko3.fifo.head = 0;
 	}
@@ -261,7 +250,9 @@ irqreturn_t k3_irq(int irq,void *dev_id,struct pt_regs *regs)
 		// That is not a problem.
 		// One more contention, If suspend == 0, and user goes and sets it up. That, is not also problem.But, suspend ==1, we get in, make it zero, wake user up, 
 		// The user goes and does 
+		
 		kyouko3.suspend = 0; //Clears the  suspension of user
+		
 	//	spin_unlock(&SMP_lock); 
 		wake_up_interruptible(&dma_snooze); 
 // User will contain on spinlock till irq is returned. 
@@ -302,8 +293,10 @@ void start_transfer(void)
 		// Now we will wait on spin_lock below to allow interrupt to finish clean. 
 		// Suppose, instead of this, many interrupts come and go between waiting period of user, (for any reason): Buffers will be empty all way. 
 		// But that is not a problem. 
+
 		wait_event_interruptible(dma_snooze,kyouko3.suspend==0);
 		// Interrupt routine will make suspend == 0. Till then user sleeps
+
 		spin_lock_irqsave(&SMP_lock,kyouko3.flags);
 		// Take the lock back before exiting the loop
 		// What happens when user comes out?
@@ -312,6 +305,7 @@ void start_transfer(void)
 		// Should we wait till fill==drain in unbind? Implemented.
 		// Without the spin lock here: We may have interrupt which clears the suspend and perform wake_up even before user is on sleep;
 		// Then our function comes in and put the user to wait forver!!
+
 	}
 }
 
@@ -322,133 +316,127 @@ long kyouko3_ioctl(struct file *fp,unsigned int cmd, unsigned long arg)
 	switch(cmd)
 	{
 		case UNBIND_DMA:
-		{
-//			printk(KERN_ALERT "UNBIND: Waiting till buffers empty");
-//			while (kyouko3.dma_fill != kyouko3.dma_drain); // This is busy wait will interrupts come in and take care of empting bufferspace.
-			// Is it needed? Who knows.
-//			printk(KERN_ALERT "UNBIND: Waiting completed on buffer_empty");
-//			K_WRITE_REG(Status,0xf);
-//			K_WRITE_REG(InterruptSet,0);
-//			pci_disable_msi(kyouko3.dev);
-//			if(!kyouko3.buffers_alloted)
-//				break;
-			for(i = 0; i < NO_OF_BUFFS; i++)
-      			{
-        			vm_munmap((unsigned long)dma_buffers[i].k_buffer_addr, (size_t) 124*1024);
-        			pci_free_consistent(kyouko3.dev, 124*1024, dma_buffers[i].k_buffer_addr ,dma_buffers[i].handle);
-      			}
-				K_WRITE_REG(InterruptSet,0);
-				free_irq(kyouko3.dev->irq,&kyouko3);
-				pci_disable_msi(kyouko3.dev);
-				
-				kyouko3.buffers_alloted =0;
-      		break;
-    	}
+//						printk(KERN_ALERT "UNBIND: Waiting till buffers empty");
+	
+						for(i = 0; i < NO_OF_BUFFS; i++)
+      					{
+      			  			vm_munmap((unsigned long)dma_buffers[i].k_buffer_addr, (size_t) 124*1024);
+        					pci_free_consistent(kyouko3.dev, 124*1024, dma_buffers[i].k_buffer_addr ,dma_buffers[i].handle);
+      					}
+						K_WRITE_REG(InterruptSet,0);
+						free_irq(kyouko3.dev->irq,&kyouko3);
+						pci_disable_msi(kyouko3.dev);
+						kyouko3.buffers_alloted =0;
+      					break;
+    	
 		case BIND_DMA:
-		{
-//			printk(KERN_DEBUG "Binding DMA");
-			for(i=0;i< NO_OF_BUFFS ; i++)
-			{
-				dma_buffers[i].k_buffer_addr = pci_alloc_consistent(kyouko3.dev, 124*1024, &(dma_buffers[i].handle));
-				dma_buffers[i].count=0;
-				dma_buffers[i].u_buffer_addr = vm_mmap(fp, ((unsigned long)(dma_buffers[i].handle)),124*1024, PROT_READ|PROT_WRITE, MAP_SHARED, dma_buffers[i].handle);	
-			}
+//						printk(KERN_DEBUG "Binding DMA");
+						for(i=0;i< NO_OF_BUFFS ; i++)
+						{
+							dma_buffers[i].k_buffer_addr = pci_alloc_consistent(kyouko3.dev, 124*1024, &(dma_buffers[i].handle));
+							dma_buffers[i].count=0;
+							dma_buffers[i].u_buffer_addr = vm_mmap(fp, ((unsigned long)(dma_buffers[i].handle)),124*1024, PROT_READ|PROT_WRITE, MAP_SHARED, dma_buffers[i].handle);	
+						}
 		
-			kyouko3.dma_fill=0;
-			kyouko3.dma_drain=0;
-			kyouko3.buffers_alloted = 1;
-			ret = pci_enable_msi(kyouko3.dev);
-			if (ret){	
-//				printk(KERN_EMERG "Bind DMA failed, returning error code");
-				return -1;
-			}
-
-			ret = request_irq(kyouko3.dev->irq,(irq_handler_t) k3_irq,IRQF_SHARED,"k3_irq",&kyouko3);
-			if(ret)
-			{
-//				printk(KERN_EMERG "Bind DMA failed, returning error code");
-				pci_disable_msi(kyouko3.dev);
-				return -1;
-			}
-
-			K_WRITE_REG(Status,0xffffffff);
-			K_WRITE_REG(InterruptSet,2);
-
-			check = copy_to_user((int *) arg, &(dma_buffers[0].u_buffer_addr), sizeof(unsigned int));
-			if(check)
-			{
-				printk(KERN_ALERT "BIND_DMA:Copy to user fails");
-			}
-			break;
-		}
+						kyouko3.dma_fill=0;
+						kyouko3.dma_drain=0;
+						kyouko3.buffers_alloted = 1;
+						ret = pci_enable_msi(kyouko3.dev);
+						if (ret)
+						{	
+//								printk(KERN_EMERG "Bind DMA failed, returning error code");
+								return -1;
+						}
+						ret = request_irq(kyouko3.dev->irq,(irq_handler_t) k3_irq,IRQF_SHARED,"k3_irq",&kyouko3);
+						if(ret)
+						{
+//							printk(KERN_EMERG "Bind DMA failed, returning error code");
+							pci_disable_msi(kyouko3.dev);
+							return -1;
+						}
+						K_WRITE_REG(Status,0xffffffff);
+						K_WRITE_REG(InterruptSet,2);
+						check = copy_to_user((int *) arg, &(dma_buffers[0].u_buffer_addr), sizeof(unsigned int));
+						if(check)
+						{
+								printk(KERN_ALERT "BIND_DMA:Copy to user fails");
+						}
+						break;
+		
 		case START_DMA:
-		{
-			spin_lock_irqsave(&SMP_lock,kyouko3.flags);
-      		ret = copy_from_user( &dma_buffers[kyouko3.dma_fill].count, (unsigned int *) arg, sizeof(unsigned int));
-			if(ret)  printk(KERN_ALERT "Start_DMA: copy from user failed");	
-			start_transfer(); // Actual start dma
-			ret = copy_to_user((int *) arg, &(dma_buffers[kyouko3.dma_fill].u_buffer_addr), sizeof(unsigned int));
-			if(ret)  printk(KERN_ALERT "Start_DMA: copy_to_user failed");
+		
+						spin_lock_irqsave(&SMP_lock,kyouko3.flags);
+      					ret = copy_from_user( &dma_buffers[kyouko3.dma_fill].count, (unsigned int *) arg, sizeof(unsigned int));
+						if(ret)
+						{  
+							printk(KERN_ALERT "Start_DMA: copy from user failed");
+						}
+						start_transfer(); // Actual start dma
+						ret = copy_to_user((int *) arg, &(dma_buffers[kyouko3.dma_fill].u_buffer_addr), sizeof(unsigned int));
+						if(ret)
+						{
+							printk(KERN_ALERT "Start_DMA: copy_to_user failed");
+						}
 
-			// Note that start_dma() changes dma_fill 
-			spin_unlock_irqrestore(&SMP_lock,kyouko3.flags);	
-			break;
-		}
+						// Note that start_dma() changes dma_fill 
+						spin_unlock_irqrestore(&SMP_lock,kyouko3.flags);	
+						break;
+		
 		case VMODE:	
-			if(arg == GRAPHICS_ON)
-			{
-				printk(KERN_ALERT "IN graphics on\n");
-				K_WRITE_REG(0x8000+ _FColumns,1024);
-				K_WRITE_REG(0x8000+_FRows,768);
-				K_WRITE_REG(0x8000+_FRowPitch,1024*4);
-				K_WRITE_REG(0x8000+_FFormat,0xf888);
-				K_WRITE_REG(0x8000+_FAddress,0);
+						if(arg == GRAPHICS_ON)
+						{
+							printk(KERN_ALERT "IN graphics on\n");
+							K_WRITE_REG(0x8000+ _FColumns,1024);
+							K_WRITE_REG(0x8000+_FRows,768);
+							K_WRITE_REG(0x8000+_FRowPitch,1024*4);
+							K_WRITE_REG(0x8000+_FFormat,0xf888);
+							K_WRITE_REG(0x8000+_FAddress,0);
 				
-				K_WRITE_REG(0x9000+_DWidth,1024);
-				K_WRITE_REG(0x9000+_DHeight,768);
-				K_WRITE_REG(0x9000+_DVirtX,0);
-				K_WRITE_REG(0x9000+_DVirtY,0);
-				K_WRITE_REG(0x9000+_DFrame,0);
-				K_WRITE_REG(Acceleration,0x40000000);
+							K_WRITE_REG(0x9000+_DWidth,1024);
+							K_WRITE_REG(0x9000+_DHeight,768);
+							K_WRITE_REG(0x9000+_DVirtX,0);
+							K_WRITE_REG(0x9000+_DVirtY,0);
+							K_WRITE_REG(0x9000+_DFrame,0);
+							K_WRITE_REG(Acceleration,0x40000000);
 				
-				udelay(2);
-				K_WRITE_REG(ModeSet,0);
-				udelay(10);
-				fifo_write(Clear_Color,0x3F000000);//load clear color
-				fifo_write(Clear_Color+4,0x3F000000);
-				fifo_write(Clear_Color+8,0x3F000000);
-				fifo_write(Clear_Color+12,0x3F800000);
-				fifo_write(Clear_Buffer,0x03);
+							udelay(2);
+							K_WRITE_REG(ModeSet,0);
+							udelay(10);
+							fifo_write(Clear_Color,0x3F000000);//load clear color
+							fifo_write(Clear_Color+4,0x3F000000);
+							fifo_write(Clear_Color+8,0x3F000000);
+							fifo_write(Clear_Color+12,0x3F800000);
+							fifo_write(Clear_Buffer,0x03);
 
-				fifo_write(Flush,0x00);
-				udelay(10);
-				fifo_flush();
-				
-				printk(KERN_ALERT "IN graphics on debug2\n");
-				kyouko3.graphics_on = 1;
-			}
-			else
-			{
-				kyouko3.graphics_on = 0;
-				fifo_flush();
-				K_WRITE_REG(Acceleration,0x80000000);
-				K_WRITE_REG(ModeSet,0);
-			}
-			break;
-		case FIFO_QUEUE:
-			ret = copy_from_user((void*)&cur_entry,(struct FIFO_entry*) arg,sizeof(struct FIFO_entry));
-			if(ret)
-				printk(KERN_EMERG "fifo_queue copy_from_user malfunction!");
-			fifo_write(cur_entry.command, cur_entry.value);
-		break;
+							fifo_write(Flush,0x00);
+							udelay(10);
+							fifo_flush();
+							kyouko3.graphics_on = 1;
+						}
+						
+						else
+						{
+							kyouko3.graphics_on = 0;
+							fifo_flush();
+							K_WRITE_REG(Acceleration,0x80000000);
+							K_WRITE_REG(ModeSet,0);
+						}
+						break;
+						
+		case FIFO_QUEUE:ret = copy_from_user((void*)&cur_entry,(struct FIFO_entry*) arg,sizeof(struct FIFO_entry));
+						if(ret)
+						{	
+							printk(KERN_EMERG "fifo_queue copy_from_user malfunction!");
+						}
+						fifo_write(cur_entry.command, cur_entry.value);
+						break;
 
-		case FIFO_FLUSH:
-			fifo_flush();
-		break;
+		case FIFO_FLUSH:fifo_flush();
+						break;
 
-		default:
-		break;
+		default:		break;
 	}
+	
 	return 0;
 }
 
@@ -456,16 +444,9 @@ int kyouko3_probe(struct pci_dev *pci_dev, const struct pci_device_id *pci_id)
 {
 	kyouko3.p_control_base = pci_resource_start(pci_dev,1);
 	kyouko3.p_card_ram = pci_resource_start(pci_dev,2);
-
 	kyouko3.len_ctrl = pci_resource_len(pci_dev,1);
 	kyouko3.len_ram = pci_resource_len(pci_dev,2);
 	kyouko3.dev = pci_dev;
-	/*if(pci_enable_device(pci_dev))
-	{
-		printk(KERN_ALERT "Error probe\n");
-	}
-	pci_set_master(pci_dev);*/
-
 	return 0;
 }
 
@@ -477,19 +458,15 @@ struct file_operations kyouko3_fops = {
 	.owner = THIS_MODULE
 };
 
-struct pci_device_id kyouko3_dev_ids[]={
-{
-
-	PCI_DEVICE(pci_vendor_ids_CCORSI,PCI_DEVICE_ID_CCORCI_KYOUKO3)
-},{0}
-};
+struct pci_device_id kyouko3_dev_ids[]={{PCI_DEVICE(pci_vendor_ids_CCORSI,PCI_DEVICE_ID_CCORCI_KYOUKO3)},{0}};
  
 void kyouko3_remove(struct pci_dev* pci_dev)
 {
 	pci_disable_device(pci_dev);
 }
 
-struct pci_driver kyouko3_pci_dev ={
+struct pci_driver kyouko3_pci_dev =
+{
 	.name = "kyouko3",
 	.id_table = kyouko3_dev_ids,
 	.probe = kyouko3_probe,
@@ -501,19 +478,15 @@ int __init kyouko_init(void)
 	cdev_init( &kyouko3_cdev, &kyouko3_fops);
 	cdev_add(  &kyouko3_cdev, MKDEV(500,127), 1);
 	kyouko3_cdev.owner = THIS_MODULE;
-
 	if(pci_register_driver(&kyouko3_pci_dev))
 	{
 		printk(KERN_ALERT "Error\n");
 	}
-
 	if(pci_enable_device(kyouko3.dev))
 	{
 		printk(KERN_ALERT "PCI ENABLE error\n");
 	}
-
 	pci_set_master(kyouko3.dev);
-	
 	printk(KERN_ALERT "Kyouko3  Initialized\n");
 	return 0;
 }
